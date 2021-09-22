2ba89d75346e03a54765b96fb1dec4a7a9a8b124
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-22 15:05:54 -0400

    Modularizing and refactoring

---
+++ input.js
@@ -0,0 +1,20 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (input) {
+  // your code here
+  if (input === '\u0003') {
+    process.exit();
+  };
+  connection.write(`Say: ${input}`);
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const connect = require("./client");
+const setupInput = require('./input');
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (input) {
-  // your code here
-  if (input === '\u0003') {
-    process.exit();
-  };
-  connection.write(`Say: ${input}`);
-};
 
 setupInput();
