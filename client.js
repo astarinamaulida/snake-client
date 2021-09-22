adad27b5d9461a4fb33b431d5000931044660e37
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-22 13:44:02 -0400

    Add name

--- client.js
+++ client.js
@@ -2,6 +2,7 @@
 const net = require("net");
 
 const connect = function () {
+  const name = 'Name: Astalavistabeybeh'
   const conn = net.createConnection({
     host: '135.23.223.133', // IP address here,
     port: '50542' // PORT number here,
@@ -10,8 +11,10 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.write(`${name}`);
+
   conn.on("connect", () => {
-    console.log('You are connected!')
+    console.log('You are connected! Enjoy the game!')
   });
 
   conn.on('data', (data) => {
